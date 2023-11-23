a682474aae686c27a619ea7426f991ac0c81dd00
kiran kundu <kiran@meliodus.org>
2023-11-23 11:42:33 +0530

    feat(style): table add in app page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,6 +3,7 @@ import style from '../styles/app.module.css'
 
 // import Deploy from '../assets/deploy.png'
 // import Add from '../assets/add.png'
+import Rightarrow from '../assets/rightarrow.png'
 
 import Sidebar from './Sidebar'
 
@@ -46,10 +47,55 @@ function App() {
         <div className={style.tablecontainer}>
           <div className={style.tablesubcontainer}>
             <div className="border-b pb-3">
-              <p className="text-default font-medium text-lg">List of instances</p>
-              <p className="text-sm text-default my-1">
-                Cloud Fox will automatically provision DNS records and wildcard certificates for all subdomains.
-              </p>
+              <div>
+                <p className="text-default font-medium text-lg">List of instances</p>
+                <p className="text-sm text-default my-1">
+                  Cloud Fox will automatically provision DNS records and wildcard certificates for all subdomains.
+                </p>
+              </div>
+            </div>
+            <div className="px-4 pt-8 sm:px-0 table-rc-sec">
+              <div className="rdb-mobile-table border-x border-t rounded table-rc ">
+                <table className="table-auto rdb-mobile  w-full text-center ">
+                  <thead className="">
+                    <tr className=" text-gray-500  text-xs text-opacity-75 border-b text-center">
+                      <th className="pl-2 py-3.5">Website name</th>
+                      <th>Published on</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody className="text-left">
+                    <tr className=" text-table_data border-b  text-sm capitalize text-opacity-75">
+                      <td data-colname="website" className="pl-2 py-2 font-semibold text-sm leading-6">
+                        resplendent-shortbread-9405a8 <br />
+                        <span className="text-[13px] font-normal text-btnPrimary">
+                          https://resplendent-shortbread-9405a8.netlify.app
+                        </span>
+                      </td>
+                      <td data-colname="Date" className="font-medium">
+                        Jun 23, 2023
+                      </td>
+                      <td data-colname="" className="font-medium">
+                        <img className="w-auto h-4 mx-auto" src={Rightarrow} alt="" />
+                      </td>
+                    </tr>
+                    <tr className=" text-table_data border-b  text-sm capitalize text-opacity-75">
+                      <td data-colname="website" className="pl-2 py-2 font-semibold text-sm leading-6">
+                        resplendent-shortbread-9405a8 <br />
+                        <span className="text-[13px] font-normal text-btnPrimary">
+                          https://resplendent-shortbread-9405a8.netlify.app
+                        </span>
+                      </td>
+                      <td data-colname="Date" className="font-medium">
+                        Jun 23, 2023
+                      </td>
+                      <td data-colname="" className="font-medium">
+                        <img className="w-auto h-4 mx-auto" src={Rightarrow} alt="" />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
           <div className="flex justify-center">
